d9e0e887d31ce2892cc6615d9edeb62e00485ad8
RhenaudTheLukark <rhen33390@gmail.com>
2026-06-14 19:17:15 +0200

    Make it so Pilot Dire Actions always happen, even on Vehicle actions

--- module/blades-actor.js
+++ module/blades-actor.js
@@ -236,9 +236,8 @@ export class BladesActor extends Actor {
             case 'actionRoll':
             case 'groupAction':
               let attribute = BladesHelpers.getAttributeFromAction(attributeName);
-              if (['insignt', 'prowess', 'resolve'].includes(attribute))
-                extraFields.dire = this.system.stress.value == this.system.stress.max;
-              else if (this.hasValidVehicle())
+              extraFields.dire = this.system.stress.value == this.system.stress.max;
+              if (['expertise', 'acuity'].includes(attribute) && this.hasValidVehicle())
                 extraFields.vehicle_dire = !Object.values(this.getVehicleData('system.quirks')).find(q => q.usable);
               extraFields.last_stand = this.system.modifiers.last_stand;
               extraFields.group_action = groupActionData;
